0071ea540dec19d3350ecd7d562fa3f0fc77ccec
dmcclendon56 <mcclendon.darrian@gmail.com>
2022-10-12 12:02:40 -0500

    adding projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
 
 const Portfolio = () => {
   return (
@@ -9,11 +13,36 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
-          <div className='portfolio__item-image'></div>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='' />
+          </div>
             <h3>This is a portfolio item title</h3>
-            <a href='https://github.com' className='btn'>Github</a>
-            <a href='https://github.com' className='btn btn-primary'>Demo</a>
-
+            <a href='https://github.com' target= 'blank' className='btn'>Github</a>
+            <a href='https://github.com' target= 'blank'  className='btn btn-primary'>Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt='' />
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <a href='https://github.com' target= 'blank' className='btn'>Github</a>
+            {/* <a href='https://github.com' target= 'blank'  className='btn btn-primary'>Demo</a> */}
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG3} alt='' />
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <a href='https://github.com' target= 'blank' className='btn'>Github</a>
+            <a href='https://github.com' target= 'blank'  className='btn btn-primary'>Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG4} alt='' />
+          </div>
+            <h3>This is a portfolio item title</h3>
+            <a href='https://github.com' target= 'blank' className='btn'>Github</a>
+            <a href='https://github.com' target= 'blank'  className='btn btn-primary'>Demo</a>
         </article>
       </div>
     </section>
